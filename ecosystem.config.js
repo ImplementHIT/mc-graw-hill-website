42e33373c3f3b55f5c4eef6b70a2b7b888370fb0
Julio Solis <julio.cesar@247labs.com>
2020-11-11 09:15:13 -0600

    Change PM2 Process name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'MyPathSurgery',
+        name: 'RotationPrep',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
